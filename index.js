1cb638133924eb063ef99e621f64594edef4682c
James Baxley <james@whitefloordesign.com>
2015-09-23 13:28:32 -0400

    left the fun begin

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-var postcss = require("postcss");
 
-module.exports = postcss.plugin("postcss-spiffing", function (opts) {
-    return function(css) {
-      css.eachDecl(function(decl) {
-        decl.prop = decl.prop.replace(/colour/g, "color");
+var _             = require('lodash');
+var postcss       = require('postcss');
+var trumpProperties = require('./lib/trump-css-properties');
+var trumpValues     = require('./lib/trump-css-values');
 
-        if(["text-align", "justify-content", "align-items", "align-content", "align-self"].indexOf(decl.prop) !== -1 && decl.value === "centre") {
-          decl.value = "center";
-        } else if(decl.prop === "font-weight" && decl.value === "plump") {
-          decl.value = "bold";
-        } else if(decl.prop === "transparency") {
-          decl.prop = "opacity";
-          if(Number(decl.value) == decl.value && (parseFloat(decl.value) <= 1 && parseFloat(decl.value) >= 0)) {
-            decl.value = (1 - parseFloat(decl.value)).toFixed((Number(decl.value) + '').replace('.', '').length -1);
-          }
-        } else if(decl.prop === "text-transform" && decl.value === "capitalise") {
-          decl.value = "capitalize";
-        } else if(decl.prop === "color" || decl.prop === "background-color" || decl.prop === "border" || decl.prop === "border-color") {
-          decl.value = decl.value.replace(/grey/g, "gray");
-        } else if(decl.prop === "background-photograph") {
-          decl.prop = "background-image";
-        }
+module.exports = postcss.plugin('postcss-trumpisms-stylesheets', function (opts) {
 
-        if(decl.value.match(/!please$/)) {
-          decl.value = decl.value.substring(0, decl.value.length-7).trim();
-          decl.important = true;
-        }
+    // your own declarations can be added in this object
+    opts = opts || {};
+    //                    key : value
+    //   'animation-direction': 'animashun-direcshun'  trumpproperties
+    //                 'right': 'rite'                 trumpvalue
 
-        decl.value = decl.value.replace(/(var\(--[^\)]*)colour([^\)]*\))/g, "$1color$2");
-      });
+    return function (css) {
 
-      css.eachAtRule(function(rule) {
-        if(rule.name === "medium") {
-          rule.name = "media";
-        }
-      });
-    }
+        css.walkDecls(function transformdecl(decl) {
+
+            // Properties
+            _.forEach(trumpProperties, function (value, key) {
+                if (decl.prop === value) {
+                    decl.prop = key;
+                }
+            });
+
+            // Values
+            _.forEach(trumpValues, function (value, key) {
+                decl.value = decl.value.replace(value, key);
+            });
+
+            // !Important
+            if (decl.value.indexOf('!fired') >= 0) {
+                decl.value = decl.value.replace(/\s*!fired\s*/, '');
+                decl.important = true;
+            }
+        });
+
+    };
 });
